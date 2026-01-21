b08e6f34b5539eb052c71514bf57fd10ed66cf2e
ShadowYourFriend <124323533+ShadowYourFriend@users.noreply.github.com>
2026-01-21 19:04:43 +0800

    Implement execute function in execution.py

---
+++ execution.py
@@ -0,0 +1,9 @@
+import actions.action
+import runtime
+from action import do
+async def execute(*args,*kwargs):
+  do.require("third-action")
+  do execute.requirement\
+
+runtime = r()
+r.run(execute(async,notion,files,*))
