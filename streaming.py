a23c9e535812fad2d016f24da5a107d6803abba4
ShadowYourFriend <124323533+ShadowYourFriend@users.noreply.github.com>
2026-01-23 07:21:18 +0800

    Create streaming.py

---
+++ streaming.py
@@ -0,0 +1,7 @@
+stream_modes = ["ringing","vibrating","None"]
+stream_mode_defaults = {'ringing': "5am-6pm",
+                        'vibrating': "6pm-12am",
+                        'None': "12am-5am"}
+streaming_intents = ["Speech", "Allah-BASED"]
+broadcast = "Infinity"
+option_to_mute = True
